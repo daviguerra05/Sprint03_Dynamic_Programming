d575352d1b1ceaa78b26698442cba08c8892d6d4
luigiferrarasinno <luigi.ferrara@sahara.consulting>
2024-09-29 21:01:46 -0300

    professores

--- Classes/Professor.py
+++ Classes/Professor.py
@@ -1,19 +1,45 @@
-class Professor:
-    def __init__(self, id: int, nome: str, idade: int, sexo: str, registro: int):
-        self.id = id
-        self.nome = nome
-        self.idade = idade
-        self.sexo = sexo
-        self.registro = registro
+from IPython.display import display
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-    # Métodos get e set para registro
-    def getRegistro(self) -> int:
-        return self.registro
 
-    def setRegistro(self, registro: int) -> None:
-        self.registro = registro
+def receber_dados_professores():
+    return pd.read_csv('./Tabelas/professores.csv')
 
-    # Representação da classe em string
-    def __str__(self) -> str:
-        return (f"ID: {self.id}, Nome: {self.nome}, Idade: {self.idade}, Sexo: {self.sexo}, "
-                f"Registro: {self.registro}")
+class TabelaProfessores:
+    # Construtor
+    def __init__(self, dataframe) -> None:
+        self.df = dataframe
+
+    # Estatísticas descritivas ----------------------------------------------------------
+    # Por Sexo
+    def media_idade_sexo(self):
+        display(self.df.groupby('Sexo')['Idade'].mean().reset_index())
+    
+    def mediana_idade_sexo(self):
+        display(self.df.groupby('Sexo')['Idade'].median().reset_index())
+
+    # Desempenho Geral
+    def media_geral_idade(self):
+        m = self.df['Idade'].mean()
+        print(f'Média de idade dos professores: {m}')
+
+    def maiores_idades(self):
+        print('\nAs 10 maiores idades dos professores:')
+        display(self.df.nlargest(10, 'Idade'))
+    
+    def distribuicao_idade(self):
+        sns.histplot(data=self.df, x='Idade')
+        plt.title('Distribuição da Idade dos Professores')
+        plt.xlabel('Idade')
+        plt.ylabel('Frequência')
+        plt.show()
+
+# Exemplo de uso:
+df_professores = receber_dados_professores()  # Recebe os dados através da função
+
+tabela_professores = TabelaProfessores(df_professores)  # Instancia a tabela com os dados recebidos
+tabela_professores.media_geral_idade()  # Calcula e exibe a média de idade
+tabela_professores.maiores_idades()  # Exibe as 10 maiores idades
+tabela_professores.distribuicao_idade()  # Plota a distribuição de idades
